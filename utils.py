73bee8c0c200150dc7a9d873697adc1485811250
8-Bit Michael <mowz2012@gmail.com>
2025-08-20 19:44:07 -0700

    Updated: utils.py
    I added some error handling utilities which other modules could access in cases where there was an error with the array or with the user attempting to modify the array during the sorting process (still a work in progress).

--- utils.py
+++ utils.py
@@ -2,3 +2,24 @@ def generate_array(min_val, max_val, size):
     import random
     return [random.randint(min_val, max_val) for _ in range(size)]
 
+def arr_error_window():
+    import tkinter as tk
+    # Describes the purpose of the script:
+    global root
+    root = tk.Tk()
+    root.geometry("650x300")
+    root.title(f"Array Error")
+    
+    label = tk.Label(root, text=f"Error: You cannot create an array with under two elements.", font=('Arial', 16, 'bold'))
+    label.pack(padx=0, pady=125)
+
+def sorting_error_window():
+    import tkinter as tk
+    # Describes the purpose of the script:
+    global root
+    root = tk.Tk()
+    root.geometry("650x300")
+    root.title(f"Sorting Error")
+    
+    label = tk.Label(root, text=f"Error: You cannot modify the graph while sorting.", font=('Arial', 16, 'bold'))
+    label.pack(padx=0, pady=125)
