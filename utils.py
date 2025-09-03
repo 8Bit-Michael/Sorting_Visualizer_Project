4d55f97a9521ae1a9bcbad44f59c5eee91ca1971
8-Bit Michael <mowz2012@gmail.com>
2025-09-03 19:05:20 -0400

    Updated: utils.py
    I removed the sorting error window that was originally made in order for a more uninterrupted experience due to it making the code unnecessarily complicated and unrealistic for my deadline.

--- utils.py
+++ utils.py
@@ -13,13 +13,3 @@ def arr_error_window():
     label = tk.Label(root, text=f"Error: You cannot create an array with under two elements.", font=('Arial', 16, 'bold'))
     label.pack(padx=0, pady=125)
 
-def sorting_error_window():
-    import tkinter as tk
-    # Describes the purpose of the script:
-    global root
-    root = tk.Tk()
-    root.geometry("650x300")
-    root.title(f"Sorting Error")
-    
-    label = tk.Label(root, text=f"Error: You cannot modify the graph while sorting.", font=('Arial', 16, 'bold'))
-    label.pack(padx=0, pady=125)
